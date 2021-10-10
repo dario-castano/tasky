44cea54589fd4360129336667270d39a756f55fe
Dario Castaño <dario.castano@aim.com>
2021-10-10 16:19:35 -0500

    Add tailwindcss support

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,15 @@ config :tasky, TaskyWeb.Endpoint,
   secret_key_base: "SLk8LQYl9BYWrtrlkYa6ep0LmK8I4sgAOxMU+HmL5vRveIcIB3jhAWbqnQFladaF",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
