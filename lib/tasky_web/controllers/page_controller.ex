e47711ea7a18aac9474256e24e894c07d8ddca69
Dario Castaño <dario.castano@aim.com>
2021-10-15 18:53:27 -0500

    Add notebooks to start page

--- lib/tasky_web/controllers/page_controller.ex
+++ lib/tasky_web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule TaskyWeb.PageController do
   use TaskyWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+
+    current_user = conn.assigns.current_user
+
+    if current_user do
+      redirect(conn, to: "/notebooks")
+    else
+      render(conn, "index.html")
+    end
   end
 end
